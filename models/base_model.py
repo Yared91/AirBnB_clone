1759667d5a446c9b961ac06e2e61f59579b0fbba
Yared91 <ykebede010@gmail.com>
2024-02-06 00:57:00 -0800

    AirBnB_clone

---
+++ models/base_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import models
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """BaseModel is the super of all the inherited classes.
+    arguments:
+    id: that assignes unique id
+    created_at: time of account creation
+    updated_at: time of account update
+    __init__: initialization
+    __str__: formated string
+    __save(self)
+    __to_dict(self)
+    *args: won't be used
+    **kwargs: dictionary
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new Base Id."""
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if "created_at" == key:
+                    self.__dict__[key] = datetime.strptime(value,
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.__dict__[key] = datetime.strptime(value,
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+        def __str__(self):
+            """Prints formated string."""
+
+            name = self.__class__.__name__
+            return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+
+        def save(self):
+            """updates the public instance attribute."""
+
+            self.updated_at = datetime.now()
+            models.storage.save()
+
+        def to_dict(self):
+            """returns dictionary containing all keys/values."""
+
+            new_dict = {
+                    "created_at": self.created_at.isoformat(),
+                    "updated_at": self.updated_at.isoformat(),
+                    "__class__": self.__class__.__name__
+                    }
+            return new_dict
