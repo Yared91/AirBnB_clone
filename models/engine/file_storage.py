8478cf371e4c34eb1484215430e50b66590b538b
Yared91 <ykebede010@gmail.com>
2024-02-06 03:39:58 -0800

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,46 +11,45 @@ import json
 
 
 class FileStorage:
-	"""
-		FileStorage Class
+    """
+    FileStorage Class
 
+    Attributes:
+    __file_path(str): file.json
+    __objects: dictionary
+    all(self): return dictionary
+    new(self, obj): new object entered
+    save(self): saves object
+    reload(self): it reloads for new entry
+    """
 
-		Attributes:
-		__file_path(str): file.json
-		__objects: dictionary
-	        all(self): return dictionary
-		new(self, obj): new object enterd
-		save(self): saves object
-		reload(self): it reloads for new entry
-	"""
-	__file_path = file.json
-	__objects = {}
+    __file_path = file.json
+    __objects = {}
 
-	def all(self):
-		"""Returns the dictionary __objects"""
-		return FileStorage.__objects
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
 
-	def new(self, obj):
-		""""sets in __objects the obj with key <obj class name>.id"""
-		obj = Filestorage.__objects["{}.{}"
-		     .format(obj.__class__.__name__, obj.id)]
+    def new(self, obj):
+        """"sets in __objects the obj with key <obj class name>.id"""
+        obj = Filestorage.__objects["{}.{}"
+                .format(obj.__class__.__name__, obj.id)]
 
-	def save(self):
-		"""serializes __objects to the JSON file (path: __file_path)"""
-		dic_json = {obj: FileStorage.__objects[obj].to_dict()
-			for obj in FileStorage.__objects.keys()}
-		with open(FileStorage.__file_path, "w") as f:
-                    json.dump(dic_json, f)
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        dic_json = {obj: FileStorage.__objects[obj].to_dict()
+                for obj in FileStorage.__objects.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(dic_json, f)
 
-	def reload(self):
-		"""deserializes the JSON file to __objects"""
-		try:
-		    fstorage = FileStorage.__file_path
-		    with open(fstorage) as f:
-			    dic_json = json.load(f)
-			    for k in dic_json.values():
-				    del k["__class__"]
-				    self.new(eval(k["__class__"])(**k))
-		except FileNotFoundError:
-			pass
-		
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            fstorage = FileStorage.__file_path
+            with open(fstorage) as f:
+                dic_json = json.load(f)
+                for k in dic_json.values():
+                    del k["__class__"]
+                    self.new(eval(k["__class__"])(**k))
+        except FileNotFoundError:
+            pass
