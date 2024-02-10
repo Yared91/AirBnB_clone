f101ff78f500baa697ca321da179b21226307920
Yared91 <ykebede010@gmail.com>
2024-02-10 04:43:07 -0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python3`
+#!/usr/bin/python3
 """ __init__ creates a unique file storage instance"""
-import models
 from models.engine.file_storage import FileStorage
+from .base_model import BaseModel
+from .user import User
+from .state import State
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .review import Review
 
 
 storage = FileStorage()
